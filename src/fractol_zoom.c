100fe97dfab18e3aa696c0ff73c12836b35ac5b3
kdavis <kyweda@gmail.com>
2017-06-22 11:07:27 -0700

    Change name of parameter in fractol_zoom

--- src/fractol_zoom.c
+++ src/fractol_zoom.c
@@ -6,20 +6,20 @@
 /*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 11:04:20 by kdavis            #+#    #+#             */
-/*   Updated: 2017/01/18 19:15:24 by kdavis           ###   ########.fr       */
+/*   Updated: 2017/06/22 11:07:05 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
 
-void	fractol_zoom(int button, t_mlx *c, int x, int y)
+void	fractol_zoom(int button, t_mlx *mlx, int x, int y)
 {
 	t_complex	cp;
 
-	frac_scale_c(c, &cp, x, y);
-	c->frc.zoom *= (button == ZOOMOT ? 1.1 : 0.9);
-	c->img.ctr.r = cp.r -
-		((double)x / (double)c->win.max_x - 0.5) * c->frc.zoom;
-	c->img.ctr.i = cp.i -
-		((double)y / (double)c->win.max_y - 0.5) * c->frc.zoom;
+	frac_scale_c(mlx, &cp, x, y);
+	mlx->frc.zoom *= (button == ZOOMOT ? 1.1 : 0.9);
+	mlx->img.ctr.r = cp.r -
+		((double)x / (double)c->win.max_x - 0.5) * mlx->frc.zoom;
+	mlx->img.ctr.i = cp.i -
+		((double)y / (double)c->win.max_y - 0.5) * mlx->frc.zoom;
 }
