6e265beee17db9903a2acfb73d301b55203563f9
kdavis <kyweda@gmail.com>
2017-06-22 12:08:48 -0700

    Change error printing to stderror

--- src/fractol_error.c
+++ src/fractol_error.c
@@ -6,14 +6,11 @@
 /*   By: kdavis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 15:18:24 by kdavis            #+#    #+#             */
-/*   Updated: 2017/01/19 11:23:18 by kdavis           ###   ########.fr       */
+/*   Updated: 2017/06/22 12:08:18 by kdavis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
 #include <fractol.h>
-#include <mlx.h>
-#include <stdlib.h>
 
 void	frac_perror(int en)
 {
@@ -25,9 +22,11 @@ void	frac_perror(int en)
 				FRACTOL7, FRACTOL8);
 	}
 	else if (en == 2)
-		ft_printf("Failed to create window\n");
+		ft_dprintf(2, "Failed to create window\n");
 	else if (en == 3)
-		ft_printf("Could not allocate enough space\n");
+		ft_dprintf(2, "Could not allocate enough space\n");
+	else if (en == 4)
+		ft_dprintf(2, "Could not open file\n");
 }
 
 void	frac_cleanup(int en, t_mlx *mlx)
@@ -39,6 +38,7 @@ void	frac_cleanup(int en, t_mlx *mlx)
 		if (mlx->win.id && mlx->id)
 			mlx_destroy_window(mlx->id, mlx->win.id);
 		ft_memdel((void*)&mlx->frc.name);
+		ft_memdel((void*)&mlx->map);
 	}
 	frac_perror(en);
 	exit(en);
